db0bd07a12ddcfc0f6ab58611b681679a21d350b
algosketch <algosketch@gmail.com>
2024-02-23 03:18:45 +0900

    Update modifier method

--- algoimageviewer/src/main/java/me/algosketch/algoimageviewer/Sample.kt
+++ algoimageviewer/src/main/java/me/algosketch/algoimageviewer/Sample.kt
@@ -1,8 +1,19 @@
 package me.algosketch.algoimageviewer
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.calculateCentroid
+import androidx.compose.foundation.gestures.calculateCentroidSize
+import androidx.compose.foundation.gestures.calculatePan
+import androidx.compose.foundation.gestures.calculateRotation
+import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,36 +22,51 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChanged
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import kotlin.math.PI
+import kotlin.math.abs
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun SampleGlideImage() {
     var scale by remember { mutableStateOf(1f) }
     var offset by remember { mutableStateOf(Offset(0f, 0f)) }
 
-    GlideImage(
-        model = "https://picsum.photos/200",
-        contentDescription = null,
+    val pagerState = rememberPagerState {
+        2
+    }
+
+    HorizontalPager(
         modifier = Modifier
-            .size(200.dp, 200.dp)
-            .pointerInput(Unit) {
-                detectTransformGestures { _, pan, zoom, _ ->
-                    scale *= zoom
+            .fillMaxSize(),
+        state = pagerState,
+    ) {
+        GlideImage(
+            modifier = Modifier.fillMaxWidth()
+                .pointerInput(Unit) {
+                    myDetectTransformGestures { _, pan, zoom, _ ->
+                        scale *= zoom
 
-                    scale = scale.coerceIn(1f, 3f)
+                        scale = scale.coerceIn(1f, 3f)
 
-                    offset = if (scale == 1f) Offset(0f, 0f) else offset + pan
+                        offset = if (scale == 1f) Offset(0f, 0f) else offset + pan
+                    }
                 }
-            }
-            .graphicsLayer {
-                scaleX = scale
-                scaleY = scale
-                translationX = offset.x
-                translationY = offset.y
-            }
-    )
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    translationX = offset.x
+                    translationY = offset.y
+                },
+            contentScale = ContentScale.FillWidth,
+            model = "https://picsum.photos/20$it",
+            contentDescription = null,
+        )
+    }
 }
